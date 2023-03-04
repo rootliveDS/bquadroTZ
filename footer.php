e3ef49a28f959f13ee4e63bf153fe6061d968ee9
rootliveDS <Daniil.Ageevv@gmail.com>
2023-03-04 15:18:59 +0300

    start tz

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Copyright &copy; 2023 Мой сайт</p>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TZ for Bquadro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header class="header">
+    <div class="logo-container">
+        <img class="logo" src="static/logo.svg" href="#" alt="Logo">
+    </div>
+    <nav class="nav-menu">
+        <ul class="nav-menu">
+            <a href="#">Главная</a>
+            <a href="#">О нас</a>
+            <a href="#">Контакты</a>
+        </ul>
+    </nav>
+</header>
